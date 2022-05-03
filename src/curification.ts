e06e7a7c9f957ace043b9e6c46bbb99756b18608
Julien Prugne <julien@webeleon.dev>
2022-05-03 08:25:45 +0200

    feat: curryfication

---
+++ src/curification.ts
@@ -0,0 +1,10 @@
+// Curryfication
+// https://www.tiktok.com/@webeleon/video/7093140557521751301
+
+function sum (a: number, b: number) {
+  return a + b;
+}
+function add5 (a: number) {
+  return sum(a, 5);
+}
+
